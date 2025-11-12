2664a2ab3451e6a1ff66da0f3eae58f69bf4412a
Oluwayomi-Olaitan <yomiolaitan03@gmail.com>
2025-11-12 23:45:26 +0000

    Family planning proportion of modern contraceptive use exploratory analysis plots

---
+++ FP_Survey_Model_Data_wrangling.R
@@ -0,0 +1,175 @@
+## loading the necessary packages
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+library(fpemlocal)
+library(countrycode)
+
+## Identifying the 85 focus countries of the FP2030 initiative
+
+# The `countrycode` package contains the division_numeric_code of all countries across the world.
+## Hence, we will use the `division_numeric_code` to extract the income groupings of these countries from the WDI.
+## Since the focus countries are mainly the low and lower middle income countries.
+
+# the division numeric code
+wdi_countries <- WDI::WDI_data$country |>
+  filter(region != "Aggregates")  |> # ignoring the aggregates
+  mutate(division_numeric_code = countrycode(iso3c, "iso3c", "un")) |>
+  select(division_numeric_code, income)
+
+
+## Other countries who joined the survey that are not part of the low and lower middle income countries of the WDI are:
+## Algeria, Belize, Botswana, El Salvador, Indonesia, Iran, Mongolia, Namibia, Ukraine.
+
+other_fcountries <- c("Algeria", "Belize", "Botswana", "El Salvador", "Indonesia",
+                      "Iran (Islamic Republic of)", "Mongolia", "Namibia", "Ukraine")
+
+
+# Let's join these to extract the grouping information of these countries and other low and lower middle income countries
+focus_countries <- divisions |>
+  select(division_numeric_code, country = name_country, region = name_region, sub_region = name_sub_region) |>
+  left_join(wdi_countries, by = "division_numeric_code") |>
+  filter(income %in% c("Low income", "Lower middle income")|
+           country %in% other_fcountries) |>
+  ungroup() |>
+  mutate(country = country |>
+           str_replace("\\s*\\(.*\\)", "") |>
+           str_replace("^Democratic People's Republic of Korea*", "DPR Korea") |>
+           str_replace("^Democratic Republic of the Congo*", "DR Congo") |>
+           str_replace("^Lao People's Democratic Republic*", "Lao PDR") |>
+           str_replace("^United Republic of\\s+", "") |>
+           str_replace("'", "’")
+  ) |>
+  filter(country != "Micronesia") # Micronesia is regarded as a sub_region, but WDI has it as a country.
+
+## `focus_countries` is a data frame containing the 85 FP2030 focus countries with their respective grouping information.
+
+
+### SURVEY DATA of the United Nations Population Division Data.
+
+## Filtering survey contraceptive use data for married women category alone of the UNPD data.
+
+contraceptive_use_data <- contraceptive_use |>
+  mutate(year = floor(start_date)) |>
+  filter(is_in_union == "Y", year >= 1990) |>
+  select(division_numeric_code,
+         year,
+         data_series_type,
+         contraceptive_use_modern, # priority
+         contraceptive_use_traditional,
+         se_modern,
+         se_traditional)
+
+## `contraceptive_use_data` above contains modern and traditional contraceptive use data across all the survey types.
+
+## `contraceptive_use` data frame have 197 country levels, filtering year >= 1990, reduced the available data to across 185 countries
+
+## ranking the survey types to prioritise DHS above others for country-year pair with more than one survey type
+
+survey_contraceptive_use_data <- contraceptive_use_data |>
+  dplyr::mutate(priority = dplyr::case_when(
+    data_series_type == "DHS" ~ 1,
+    data_series_type == "MICS" ~ 2,
+    data_series_type == "PMA" ~ 3,
+    data_series_type == "National survey" ~ 4,
+    data_series_type == "Other" ~ 5
+  )) |>
+  dplyr::group_by(division_numeric_code, year) |>
+  dplyr::slice_min(order_by = priority) |>
+  ungroup() |>
+  select(-priority) |>
+  dplyr::group_by(division_numeric_code, year, data_series_type) |>  # if the country-year have multiple values from the same survey take the average.
+  summarise_if(is.numeric, mean, na.rm = TRUE) |>
+  left_join(divisions |>
+              select(division_numeric_code, country = name_country,
+                     sub_region = name_sub_region,region = name_region),
+            join_by("division_numeric_code")) |>
+  mutate(country = country |>
+           str_replace("\\s*\\(.*\\)", "") |>
+           str_replace("^Democratic People's Republic of Korea*", "DPR Korea") |>
+           str_replace("^Democratic Republic of the Congo*", "DR Congo") |>
+           str_replace("^Lao People's Democratic Republic*", "Lao PDR") |>
+           str_replace("^United Republic of\\s+", "") |>
+           str_replace("'", "’")
+  ) |>
+  ungroup()
+
+# survey_contraceptive_use_data contains one country-year pair for all the family planning data.
+
+
+## The interest for this analysis is the family planning focus countries as specified by the track20.
+
+## The survey data for the 85 focus countries only
+
+survey_fc_contraceptive_use_data <- survey_contraceptive_use_data |>
+  filter(division_numeric_code %in% focus_countries$division_numeric_code)
+
+## Hence,`survey_fc_contraceptive_use_data` above contains modern and traditional contraceptive use data
+## with one data point per country-year combination and priority on DHS over other survey types.
+
+
+### MODEL DATA of the United Nations Population Division Family Planning Estimation Model Outputs.
+
+FPEM_model_output <- readr::read_csv("Data_FamilyPlanningIndicators_2024.csv")
+
+model_data <- FPEM_model_output |> filter(
+  Time >= 1990 & Time <= 2025, # year range
+  Variant == "Median", # Model-based Median estimates
+  Category == "Married or in a union women",
+  IndicatorShortName %in% c("CPModP", "CPTrad") # modern and traditional
+) |>
+  mutate(
+    proportion = Value / 100,
+    indicator = dplyr::recode( # renaming the indicator codes
+      IndicatorShortName,
+      "CPModP" = "contraceptive_use_modern",
+      "CPTrad" = "contraceptive_use_traditional"
+    )
+  ) |>
+  select(LocationId, year = Time, indicator, EstimateMethod, proportion)
+
+
+## joining the country grouping information to the `model_contraceptive_use_data`.
+
+model_contraceptive_use_data <- model_data |>
+  left_join(divisions |>
+              select(division_numeric_code, country = name_country,
+                     sub_region = name_sub_region,region = name_region),
+            join_by("LocationId" == "division_numeric_code")) |>
+  mutate(country = country |>
+           str_replace("\\s*\\(.*\\)", "") |>
+           str_replace("^Democratic People's Republic of Korea*", "DPR Korea") |>
+           str_replace("^Democratic Republic of the Congo*", "DR Congo") |>
+           str_replace("^Lao People's Democratic Republic*", "Lao PDR") |>
+           str_replace("^United Republic of\\s+", "") |>
+           str_replace("'", "’")
+  ) |>
+  ungroup() |>
+  pivot_wider(id_cols = c(LocationId, country, region, sub_region, year),
+              values_from = proportion,
+              names_from = indicator) |>
+  filter(!is.na(country))
+
+
+## The model-based estimates for the 85 focus countries only
+
+model_fc_contraceptive_use_data <- model_contraceptive_use_data |>
+  filter(LocationId %in% focus_countries$division_numeric_code)
+
+
+#### For reference
+#`survey_contraceptive_use_data` is the survey data of the proportion of modern
+# and traditional contraceptive uses.
+
+#`model_contraceptive_use_data` is the model data of the proportion of modern
+# and traditional contraceptive uses.
+
+#`survey_fc_contraceptive_use_data` is the survey data of the proportion of modern
+# and traditional contraceptive uses for the FP2030 focus countries.
+
+#`model_fc_contraceptive_use_data` is the model data of the proportion of modern
+# and traditional contraceptive uses for the FP2030 focus countries.
+
+# `focus_countries` is the list of the 85 FP2030 initiative focus countries.
+
+
---
+++ FP_interactive_plots.R
@@ -0,0 +1,317 @@
+library(htmlwidgets)
+library(wdiexplorer)
+
+# a folder to save interactive plots
+dir.create("interactive-plots", showWarnings = FALSE)
+
+
+## The data wrangling decisions and FP data are saved in the `FP_Survey_Model_Data_wrangling.R` script.
+
+source("FP_Survey_Model_Data_wrangling.R")
+
+## The Trend strength measure
+
+# survey data
+survey_contraceptive_use_trendstrength <- wdiexplorer::compute_trend_shape_features(
+  wdi_data = survey_fc_contraceptive_use_data, 
+  index = "contraceptive_use_modern"
+) |> 
+  select(country, trend_strength)
+
+
+survey_contraceptive_use_trendstrength_group <- wdiexplorer::add_group_info(
+  metric_summary = survey_contraceptive_use_trendstrength, 
+  wdi_data = survey_fc_contraceptive_use_data
+)
+
+#model data
+model_contraceptive_use_trendstrength <- wdiexplorer::compute_trend_shape_features(
+  wdi_data = model_fc_contraceptive_use_data, 
+  index = "contraceptive_use_modern"
+)  |> 
+  select(country, trend_strength)
+
+model_contraceptive_use_trendstrength_group <- wdiexplorer::add_group_info(
+  metric_summary = model_contraceptive_use_trendstrength, 
+  wdi_data = model_fc_contraceptive_use_data
+)
+
+
+## (a) A scatterplot of model-based estimates trend strength against survey data trend strength.
+
+combined_trend_strength <- model_contraceptive_use_trendstrength_group |>
+  select(country, sub_region, model_trend_strength = trend_strength) |>
+  left_join(survey_contraceptive_use_trendstrength_group |>
+              select(country, survey_trend_strength = trend_strength),
+            join_by("country")) |>
+  mutate(ratio = model_trend_strength / survey_trend_strength)
+
+
+# identifying the countries with the extreme differences
+trend_strength_top_10 <- combined_trend_strength |>
+  arrange(desc(ratio)) |>
+  dplyr::slice_head(n = 10) |>
+  pull(country)
+
+# the interactive scatterplot
+T <- ggplot2::ggplot(data = combined_trend_strength) + 
+  ggiraph::geom_point_interactive(
+    ggplot2::aes(x = survey_trend_strength, y = ratio, colour = sub_region, tooltip = paste0(
+      country,  "\n",
+      "Survey: ", sprintf("%.3f", survey_trend_strength), "\t",
+      ", Model: ", sprintf("%.3f", model_trend_strength)), data_id = country), size = 2.2) +
+  ggplot2::geom_hline(yintercept = 1) +
+  ggplot2::theme_bw() +
+  theme(legend.position = "bottom")
+
+
+# Interactive plot
+saveWidget(
+ggiraph::girafe(
+  ggobj = T,
+  width_svg = 10,
+  height_svg = 6,
+  options = list(
+    ggiraph::opts_hover_inv(css = "opacity: 0.1;"),  # makes non-hovered lines fade
+    ggiraph::opts_hover(css = "opacity: 1; stroke-width: 2;")  # on hover: no color change
+  )
+),
+file = "interactive-plots/trendstrength_plot.html",
+selfcontained = TRUE
+) 
+
+
+## (b) A scatterplot of model-based estimates trend strength against survey data trend strength
+
+survey_contraceptive_use_silwidth <- wdiexplorer::compute_variation(
+  wdi_data = survey_contraceptive_use_data, 
+  index = "contraceptive_use_modern",
+  group_var = "sub_region"
+) |> 
+  select(country, sil_width)
+
+
+survey_contraceptive_use_silwidth_group <- wdiexplorer::add_group_info(
+  metric_summary = survey_contraceptive_use_silwidth, 
+  wdi_data = survey_contraceptive_use_data
+)
+
+#model data
+model_contraceptive_use_silwidth <- wdiexplorer::compute_variation(
+  wdi_data = model_contraceptive_use_data, 
+  index = "contraceptive_use_modern",
+  group_var = "sub_region"
+)  |> 
+  select(country, sil_width)
+
+model_contraceptive_use_silwidth_group <- wdiexplorer::add_group_info(
+  metric_summary = model_contraceptive_use_silwidth, 
+  wdi_data = model_contraceptive_use_data
+)
+
+# filter sub_region with more than one country as the valid sub_region for the partition plot
+fc_subregions_with_multi_countries <- focus_countries |>
+  distinct(country, sub_region) |>
+  count(sub_region) |>
+  filter(n > 1) |>
+  pull(sub_region)
+
+# survey
+survey_fc_contraceptive_use_silwidth_group <- survey_contraceptive_use_silwidth_group |>
+  filter(
+    country %in% focus_countries$country,      # focus countries
+    sub_region %in% fc_subregions_with_multi_countries) # in subregion with more than 1 country
+
+# model
+model_fc_contraceptive_use_silwidth_group <- model_contraceptive_use_silwidth_group |>
+  filter(
+    country %in% focus_countries$country,      # focus countries
+    sub_region %in% fc_subregions_with_multi_countries) # in subregion with more than 1 country
+
+
+combined_sil_widths <- model_fc_contraceptive_use_silwidth_group |>
+  select(country, sub_region, model_sil_width = sil_width) |>
+  left_join(survey_fc_contraceptive_use_silwidth_group |>
+              select(country, survey_sil_width = sil_width),
+            join_by("country")) |>
+  mutate(diff = abs(model_sil_width - survey_sil_width))
+
+
+# Identifying countries with high differences
+sil_widths_top_10 <- combined_sil_widths |>
+  arrange(desc(diff)) |>
+  dplyr::slice_head(n = 10) |>
+  pull(country)
+
+S <- ggplot(data = combined_sil_widths) +
+  ggiraph::geom_point_interactive(
+    aes(x = survey_sil_width, y = model_sil_width, colour = sub_region, 
+        tooltip = paste(
+    country, "\n",
+    "Survey: ", sprintf("%.3f", survey_sil_width), "\t",
+    "Model: ", sprintf("%.3f", model_sil_width)), data_id = country),
+    size = 2.2) +
+  geom_line(aes(x = survey_sil_width, y = survey_sil_width), colour = "grey") +
+  theme_bw()
+
+# Interactive plot
+saveWidget(
+ggiraph::girafe(
+  ggobj = S,
+  width_svg = 10,
+  height_svg = 6,
+  options = list(
+    ggiraph::opts_hover_inv(css = "opacity: 0.1;"),  # makes non-hovered lines fade
+    ggiraph::opts_hover(css = "opacity: 1; stroke-width: 2;")  # on hover: no color change
+  )
+),
+file = "interactive-plots/silwidths_plot.html",
+selfcontained = TRUE
+) 
+
+
+## (c) Identifying top 10 countries with the highest differences and 
+## compare their data trajectories with their neighbouring countries in the same sub-region.
+
+
+# subregion of the top 10 countries
+sil_widths_top_10_subregion <- combined_sil_widths |>
+  arrange(desc(diff)) |>
+  dplyr::slice_head(n = 10) |>
+  pull(sub_region) |> unique()
+
+
+# The data for focus countries with more than one country in its sub-region
+sil_widths_top_10_subregion_data <- survey_fc_contraceptive_use_data |>
+  filter(
+    sub_region %in% sil_widths_top_10_subregion) # focus countries in subregion with more than 1 country
+
+
+#Joining the model-based estimates with the combine sil_widths
+
+model_fc_data_sil_width_metrics <- model_fc_contraceptive_use_data |>
+  filter(
+    sub_region %in% sil_widths_top_10_subregion) |> # focus countries in subregion with more than 1 country
+  left_join(model_fc_contraceptive_use_silwidth_group |>
+              select(country, model_sil_width = sil_width), by = "country") |>
+  left_join(survey_fc_contraceptive_use_silwidth_group |>
+              select(country, survey_sil_width = sil_width), by = "country")
+
+# the plot
+
+S2 <- ggplot2::ggplot() +
+  ggplot2::geom_point(
+    data = sil_widths_top_10_subregion_data,
+      ggplot2::aes(x = year, y = contraceptive_use_modern), 
+      size = 1.55, colour = "grey60") +
+  ggplot2::geom_point(
+    data = sil_widths_top_10_subregion_data |> 
+        filter(country %in% sil_widths_top_10),
+    ggplot2::aes(x = year, y = contraceptive_use_modern, colour = country,), 
+        size = 1.55) +
+  ggiraph::geom_line_interactive(
+    data = model_fc_data_sil_width_metrics, 
+      ggplot2::aes(x = year, y = contraceptive_use_modern, group = country, 
+            tooltip = paste(
+               country, "\n",
+               "Survey: ", sprintf("%.3f", survey_sil_width), "\t",
+               "Model: ", sprintf("%.3f", model_sil_width)), data_id = country), 
+                colour = "grey55") +
+  ggiraph::geom_line_interactive(
+    data = model_fc_data_sil_width_metrics |> 
+      filter(country %in% sil_widths_top_10), 
+    ggplot2::aes(x = year, y = contraceptive_use_modern, group = country, colour = country, 
+                 tooltip = paste(
+      country, "\n",
+      "Survey: ", sprintf("%.3f", survey_sil_width), "\t",
+      ", Model: ", sprintf("%.3f", model_sil_width)), data_id = country)) +
+  ggplot2::facet_wrap(~forcats::fct_relevel(sub_region, sil_widths_top_10_subregion), 
+             nrow = 3, scales = "free") +
+  ggplot2::ylab("Modern contraceptive use proportion") +
+  ggplot2::theme_bw() +
+  ggplot2::theme(legend.position = "none")
+
+
+# Interactive plot
+saveWidget(
+ggiraph::girafe(
+  ggobj = S2,
+  width_svg = 10,
+  height_svg = 6,
+  options = list(
+    ggiraph::opts_hover_inv(css = "opacity: 0.1;"),  # makes non-hovered lines fade
+    ggiraph::opts_hover(css = "opacity: 1; stroke-width: 2;")  # on hover: no color change
+  )
+),
+file = "interactive-plots/silwidths_trajectories_plot.html",
+selfcontained = TRUE
+)
+
+
+## (d) Filtering the corresponding predicted data for pairs where we have survey data.
+
+contraceptive_data_residuals <- survey_fc_contraceptive_use_data |>
+  select(country, year, survey_data = contraceptive_use_modern) |>
+  left_join(model_fc_contraceptive_use_data |>
+              select(country, year, region, sub_region,
+                     model_data = contraceptive_use_modern),
+            by = c("country", "year")) |>
+  mutate(residuals = model_data - survey_data)
+
+
+## Computing the linearity and curvature
+
+residual_temporal_features <- wdiexplorer::compute_trend_shape_features(
+  wdi_data = contraceptive_data_residuals,
+  index = "residuals")
+
+residual_temporal_features_group <- wdiexplorer::add_group_info(
+  metric_summary = residual_temporal_features, 
+  wdi_data = contraceptive_data_residuals
+)
+
+
+LC <- ggplot2::ggplot(
+    data = residual_temporal_features_group) +
+  ggiraph::geom_point_interactive(
+    data = residual_temporal_features_group, 
+    ggplot2::aes(x = linearity, y = curvature,
+                 tooltip = paste(country), data_id = country, colour = sub_region)
+    ) +
+  ggplot2::theme_bw() +
+  ggplot2::theme(legend.position = "bottom")
+
+# Interactive plot
+saveWidget(
+  ggiraph::girafe(
+    ggobj = LC,
+    width_svg = 10,
+    height_svg = 6,
+    options = list(
+      ggiraph::opts_hover_inv(css = "opacity: 0.1;"),  # makes non-hovered lines fade
+      ggiraph::opts_hover(css = "opacity: 1; stroke-width: 2;")  # on hover: no color change
+    )
+  ),
+  file = "interactive-plots/curvature_linearity_plot.html",
+  selfcontained = TRUE
+)
+
+
+
+
+#### creating a folder for all the plots
+
+# a list of all HTML files inside the interactive-plots folder
+files <- list.files("interactive-plots", pattern = "\\.html$", full.names = FALSE)
+
+links <- paste0("<li><a href='interactive-plots/", files, "'>", files, "</a></li>")
+
+
+family_planning_analysis_interactive_plot_html <- paste0(
+  "<!DOCTYPE html><html><head><title>Family planning modern contraceptive use exploratory data analysis interactive plots gallery</title></head><body>",
+  "<h1>Family planning modern contraceptive use exploratory data analysis interactive plots gallery</h1><ul>",
+  paste(links, collapse = "\n"),
+  "</ul></body></html>"
+)
+
+writeLines(family_planning_analysis_interactive_plot_html, "index.html")
